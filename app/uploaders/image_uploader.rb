7841d94fe0beaf742af192820a08e56c3e27881f
dvdzz <dvdzz76@icloud.com>
2019-07-08 23:50:31 +0000

    added ImageMagick gem - still issues

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
     
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
+  # include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
