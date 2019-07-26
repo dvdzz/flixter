5032cb2e6491ea8f9d9847a8dec7c7200c401780
dvdzz <dvdzz76@icloud.com>
2019-07-26 21:44:30 +0000

    finished the static pages...flixter done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,13 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def privacy
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'about' , to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
